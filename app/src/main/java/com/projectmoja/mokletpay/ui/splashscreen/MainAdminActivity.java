f2ae062b774048220b430cf342b752d1d4715ed2
Memoreza Sabana <53498578+memorezasabana@users.noreply.github.com>
2021-11-11 18:50:13 +0700

    MainAdminActivity

---
+++ app/src/main/java/com/projectmoja/mokletpay/ui/splashscreen/MainAdminActivity.java
@@ -0,0 +1,15 @@
+package com.projectmoja.mokletpay.ui.splashscreen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.projectmoja.mokletpay.R;
+
+public class MainAdminActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_admin);
+    }
+}
